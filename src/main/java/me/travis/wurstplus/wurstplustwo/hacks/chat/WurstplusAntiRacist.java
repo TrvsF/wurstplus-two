32bf453954855345e3b67d4f470e7d75e91c5683
Nathan <50296190+NathanW-Main@users.noreply.github.com>
2020-09-18 23:46:57 -0400

    #BLM #ICANTBREATH

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/chat/WurstplusAntiRacist.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/chat/WurstplusAntiRacist.java
@@ -1,16 +1,24 @@
 package me.travis.wurstplus.wurstplustwo.hacks.chat;
 
+import me.travis.wurstplus.wurstplustwo.event.events.WurstplusEventPacket;
 import me.travis.wurstplus.wurstplustwo.guiscreen.settings.WurstplusSetting;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
+import me.zero.alpine.fork.listener.EventHandler;
+import me.zero.alpine.fork.listener.Listener;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.play.client.CPacketChatMessage;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class WurstplusAntiRacist extends WurstplusHack {
-    
+
+    /*
+     *    Updated by NathanW because we need to end racism on anarchy servers
+     */
+
     public WurstplusAntiRacist() {
         super(WurstplusCategory.WURSTPLUS_CHAT);
 
@@ -20,16 +28,16 @@ public class WurstplusAntiRacist extends WurstplusHack {
     }
 
     WurstplusSetting delay = create("Delay", "AntiRacistDelay", 10, 0, 100);
+    WurstplusSetting anti_nword = create("AntiNword", "AntiRacismAntiNword", true);
+    WurstplusSetting chanter = create("Chanter", "AntiRacismChanter", false);
 
     List<String> chants = new ArrayList<>();
 
     Random r = new Random();
-
     int tick_delay;
 
     @Override
     protected void enable() {
-        
         tick_delay = 0;
 
         chants.add("<player> you fucking racist");
@@ -44,32 +52,97 @@ public class WurstplusAntiRacist extends WurstplusHack {
         chants.add("RESPECT BLM");
         chants.add("IF YOURE NOT WITH US, YOURE AGAINST US");
         chants.add("DEREK CHAUVIN WAS A RACIST");
-
     }
+
+    String[] random_correction = {
+            "Yuo jstu got nea nae'd by worst+2",
+            "Wurst+2 just stopped me from saying something racially incorrect!",
+            "<Insert nword word here>",
+            "Im an edgy teenager and saying the nword makes me feel empowered on the internet.",
+            "My mom calls me a late bloomer",
+            "I really do think im funny",
+            "Niger is a great county, I do say so myself",
+            "Mommy and daddy are wrestling in the bedroom again so im going to play minecraft until their done",
+            "How do you open the impact GUI?",
+            "What time does FitMC do his basehunting livestreams?"
+    };
+
+
+    CharSequence nigger = "nigger";
+    String hardR = nigger.toString();
+
+    CharSequence nigga = "nigga";
+    String normal = nigga.toString();
 
     @Override
     public void update() {
-        
-        tick_delay++;
-        if (tick_delay < delay.get_value(1)*10) return;
 
-        String s = chants.get(r.nextInt(chants.size()));
-        String name =  get_random_name();
-        
-        if (name == mc.player.getName()) return;
+        if(chanter.get_value(true)) {
 
-        mc.player.sendChatMessage(s.replace("<player>", name));
+            tick_delay++;
 
-        tick_delay = 0;
+            if (tick_delay < delay.get_value(1) * 10) return;
 
-    }
+            String s = chants.get(r.nextInt(chants.size()));
+            String name = get_random_name();
+
+            if (name == mc.player.getName()) return;
+
+            mc.player.sendChatMessage(s.replace("<player>", name));
+            tick_delay = 0;
+
+            }
+        }
 
     public String get_random_name() {
 
-        List<EntityPlayer> players = mc.world.playerEntities;
+            List<EntityPlayer> players = mc.world.playerEntities;
+            return players.get(r.nextInt(players.size())).getName();
+        }
 
-        return players.get(r.nextInt(players.size())).getName();
 
+    public String random_string(String[] list) {
+        return list[r.nextInt(list.length)];
     }
+
+    //Anti n-word
+   @EventHandler
+    private Listener<WurstplusEventPacket.SendPacket> listener = new Listener<>(event -> {
+
+        if (!(event.get_packet() instanceof CPacketChatMessage)) {
+            return;
+        }
+
+        if(anti_nword.get_value(true)) {
+
+            String message = ((CPacketChatMessage) event.get_packet()).getMessage().toString().toLowerCase();
+
+            String x = Integer.toString((int) (mc.player.posX));
+            String z = Integer.toString((int) (mc.player.posZ));
+
+            String coords = x + " " + z;
+
+            if (message.contains(nigger) || message.contains(nigga)) {
+
+                StringBuilder correction = new StringBuilder();
+                correction.append((random_string(random_correction)));
+
+                message = correction.toString();
+
+                StringBuilder coordsPost = new StringBuilder();
+                coordsPost.append(coords);
+
+                if (anti_nword.get_value(true)) {
+                    mc.player.connection.sendPacket(new CPacketChatMessage("Hi, im at " + coordsPost.toString() + ", come teach me a lesson about racism"));
+                }
+            } else {
+                StringBuilder nvm = new StringBuilder();
+                nvm.append((message));
+            }
+
+            ((CPacketChatMessage) event.get_packet()).message = message;
+        }
+    });
+
 
 }
